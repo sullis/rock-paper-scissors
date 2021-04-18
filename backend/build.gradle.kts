d967586a4e0869b43247429ae11eea7e341a4219
sullis <github@seansullivan.com>
2021-04-17 21:37:46 -0700

    junit 5.7.1

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -8,7 +8,7 @@ repositories {
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.1")
 
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
 
